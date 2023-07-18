360251e7919c9916a20533084ac302ca53ff5911
Ali Kemal Gündüz <kemall.gunduz@icloud.com>
2023-07-18 13:12:35 +0300

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayididizisi = { 23, 35, 9, 85, 3, 8, 15 };
+            Array.Sort(sayididizisi);
+            foreach (var i in sayididizisi)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
